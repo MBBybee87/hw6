66a32c6fb5f974416f2ec3efa8ea0712e818c95a
MBBybee87 <matthewbybee@mail.weber.edu>
2019-03-23 13:51:45 -0600

    Brought into better formating

--- matt_bybee_hw6.py
+++ matt_bybee_hw6.py
@@ -18,30 +18,32 @@ def parseArg(argument):
     """
     n = 0 #for final loop to ensure only top 25 errors are shown
     d = {} #dictionary
-    x = urllib.request.urlopen(argument) #url
+    data = urllib.request.urlopen(argument).readlines() #url
 
-    data = x.readlines()
-    for i in range(len(data)):
-        line = data[i].decode("utf-8")
-        if (re.search("\[error\]", line)):
-            result = re.split("\s", line)
-            if result[-2] in d:
+    for i in range(len(data)): #go over each line individually
+        line = data[i].decode("utf-8") #convert from BYTE to STR
+        if (re.search("\[error\]", line)): #regex determine if error
+            result = re.split("\s", line) #split on spaces
+            if result[-2] in d: #if key exists, increment value
                 d[result[-2]] += 1
-            else:
+            else: #if key does not exist, initiate key and base value of 1
                 d[result[-2]] = 1
 
     print ("*** TOP 25 PAGE ERRORS ***") 
+
     for key, value in sorted(d.items(), key=operator.itemgetter(1), reverse=True):
-        print ("Count: %9i Page: %s" % (value, key))
-        n += 1
+        #iterate over a sorted dictionary
+        print ("Count: %9i Page: %s" % (value, key)) #print result
+        n += 1 #but only top 25
         if (n == 25):
             break
         else:
             continue
+
     return
 
 #main
-try:
+try: #do try/catch for portability
     parseArg(sys.argv[1])
 except IndexError:
     help()
